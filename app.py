dd83eb016490d70a95c0d5b25b8d138aec1d1970
Nguyễn Quốc Cường <104240677@student.vgu.edu.vn>
2025-08-27 21:43:17 +0700

    edit

--- app.py
+++ app.py
@@ -25,7 +25,6 @@ import json
 from pydantic import BaseModel
 from typing import Optional
 import facer
-from huggingface_hub import hf_hub_download,snapshot_download
 
 
 
@@ -39,33 +38,6 @@ pipeline_swap = None
 face_parser = None
 executor = ThreadPoolExecutor(max_workers=1)
 
-if not os.path.exists("./models/antelopev2/"):
-    snapshot_download(
-        repo_id="InstantX/InstantID", allow_patterns="/models/antelopev2/*", local_dir="./models/antelopev2/"
-    )
-    # run 'mv models/antelopev2/antelopev2/* models/antelopev2/' cmd
-
-if not os.path.exists("./checkpoints/"):
-    # make dir 'ControlNetModel'
-    os.makedirs("./checkpoints/ControlNetModel", exist_ok=True)
-    hf_hub_download(
-        repo_id="InstantX/InstantID",
-        subfolder="ControlNetModel",
-        filename="config.json",
-        local_dir="./checkpoints"
-    )
-    
-    hf_hub_download(
-        repo_id="InstantX/InstantID",
-        subfolder="ControlNetModel",
-        filename="diffusion_pytorch_model.safetensors",
-        local_dir="./checkpoints"
-    )
-    hf_hub_download(
-        repo_id="InstantX/InstantID",
-        filename="ip-adapter.bin",
-        local_dir="./checkpoints"
-    )
 
 
 
